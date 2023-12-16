3f686e52462e898d7b6f951a19c97c24f0f1e887
weian404 <weian404@gmail.com>
2023-12-16 18:39:34 +0800

    init notification spring project

---
+++ notification-spring/src/main/java/org/opengear/notification/NotificationProjectApplication.java
@@ -0,0 +1,13 @@
+package org.opengear.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProjectApplication.class, args);
+	}
+
+}
---
+++ notification-spring/src/test/java/org/opengear/notification/NotificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opengear.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
